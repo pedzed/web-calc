21792ff3e1654b81e9d7af7a67a0e409afcf5431
ped <pedzed@outlook.com>
2014-12-03 16:41:01 +0100

    Prepared calculator design.

--- app/views/home.php
+++ app/views/home.php
@@ -10,19 +10,54 @@
         <!--[if lt IE 9]>
             <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     </head>
     <body>
         <div id="main-content">
             <form id="calculator" method="POST" action="<?= URL::route('calculator-post'); ?>">
-                <table border="1">
+                <table>
                     <tr>
-                        <td><input type="text" name="input" value="0" /></td>
+                        <td colspan="4">
+                            <input type="text"
+                                   name="input"
+                                   placeholder="0"
+                            />
+                        </td>
                     </tr>
                     <tr>
-                        <td>test</td>
+                        <td><button class="button" data-id="CE" disabled>CE</button></td>
+                        <td><button class="button" data-id="backspace" disabled>&larr;</button></td>
+                        <td><button class="button" data-id="%">%</button></td>
+                        <td><button class="button" data-id="+">+</button></td>
+                    </tr>
+                    <tr>
+                        <td><button class="button" data-id="7">7</button></td>
+                        <td><button class="button" data-id="8">8</button></td>
+                        <td><button class="button" data-id="9">9</button></td>
+                        <td><button class="button" data-id="-">-</button></td>
+                    </tr>
+                    <tr>
+                        <td><button class="button" data-id="4">4</button></td>
+                        <td><button class="button" data-id="5">5</button></td>
+                        <td><button class="button" data-id="6">6</button></td>
+                        <td><button class="button" data-id="*">*</button></td>
+                    </tr>
+                    <tr>
+                        <td><button class="button" data-id="4">1</button></td>
+                        <td><button class="button" data-id="5">2</button></td>
+                        <td><button class="button" data-id="6">3</button></td>
+                        <td><button class="button" data-id="/">/</button></td>
+                    </tr>
+                    <tr>
+                        <td><button class="button" data-id="0">0</button></td>
+                        <td><button class="button" data-id=".">.</button></td>
+                        <td colspan="2"><button class="button" data-id="=">=</button></td>
                     </tr>
                 </table>
             </form>
         </div>
+        
+        {{ script('scripts/calculator.js') }}
     </body>
 </html>
---
+++ public_html/scripts/calculator.js
@@ -0,0 +1,7 @@
+(function($){
+    
+    var calc = $('#calculator');
+    
+    console.log(calc);
+    
+})(jQuery);
